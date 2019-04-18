4ee5774285b240771e62b59df74afb319d7090f8
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2019-04-18 21:15:53 +0600

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,13 @@
+function add()
+{
+	var x1=parseFloat(document.getElementById("num1").value);
+	var x2=parseFloat(document.getElementById("num2").value);
+	
+	document.getElementById("res").innerHtml="Result is ="+(x1+x2);
+}
+function sub()
+{
+	
+}
+function mul(){}
+function div(){}
